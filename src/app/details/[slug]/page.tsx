aacbd37d8343984e3fa3752220bdc3c0626bdf58
Alex Gutierrez <a.gutierrez@orekait.com>
2025-06-18 10:25:34 +0200

    Params fix

--- src/app/details/[slug]/page.tsx
+++ src/app/details/[slug]/page.tsx
@@ -3,7 +3,7 @@ import Details from "@/components/Details/Details";
 import Navbar from "@/components/Navbar/Navbar";
 
 const DetailsPage = async ({ params }: { params: { slug: string } }) => {
-  const { slug } = await params;
+  const { slug } = params;
 
   return (
     <div className="w-full min-h-screen bg-gray-950 text-white flex flex-col">
