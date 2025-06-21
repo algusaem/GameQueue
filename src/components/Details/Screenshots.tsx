37a3d28db9d3dd62f4ae397c16ea663c6672afcf
Alex Gutierrez <algusaem@gmail.com>
2025-06-21 10:33:28 +0200

    New screenshot system

--- src/components/Details/Screenshots.tsx
+++ src/components/Details/Screenshots.tsx
@@ -1,22 +1,33 @@
+"use client";
+
 import useFetchScreenshots from "@/hooks/useFetchScreenshots";
 import Image from "next/image";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const Screenshots = ({ slug }: { slug: string }) => {
   const { data: screenshots } = useFetchScreenshots(slug);
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {screenshots?.results.map((screenshot: { id: number; image: string }) => (
-        <div key={screenshot.id} className="relative w-full h-64">
-          <Image
-            src={screenshot.image}
-            alt={`Screenshot ${screenshot.id}`}
-            fill
-            className="object-cover rounded-lg"
-            sizes="(max-width: 1024px) 100vw, 33vw"
-          />
-        </div>
-      ))}
-    </div>
+    <PhotoProvider>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {screenshots?.results.map(
+          (screenshot: { id: number; image: string }) => (
+            <PhotoView key={screenshot.id} src={screenshot.image}>
+              <div className="relative w-full h-64 cursor-pointer">
+                <Image
+                  src={screenshot.image}
+                  alt={`Screenshot ${screenshot.id}`}
+                  fill
+                  className="object-cover rounded-lg transition-transform duration-300 hover:-translate-y-1 hover:scale-[1.02]"
+                  sizes="(max-width: 1024px) 100vw, 33vw"
+                />
+              </div>
+            </PhotoView>
+          )
+        )}
+      </div>
+    </PhotoProvider>
   );
 };
 
